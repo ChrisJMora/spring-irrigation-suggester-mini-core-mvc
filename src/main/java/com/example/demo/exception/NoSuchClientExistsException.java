16325f1d350e62468c15fc9fa9407d41112cffdf
Chris <christian.jacome.mora@udla.edu.ec>
2024-10-09 23:57:00 -0500

    New Exception: NoSuchClientExistsException

---
+++ src/main/java/com/example/demo/exception/NoSuchClientExistsException.java
@@ -0,0 +1,26 @@
+package com.example.demo.exception;
+
+import lombok.NoArgsConstructor;
+
+/**
+ * Runtime exception that will be thrown when a user tries to update/delete
+ * a client that doesn't exist
+ *
+ * @author Christian Jácome
+ */
+@NoArgsConstructor
+public class NoSuchClientExistsException extends
+        RuntimeException {
+
+    private String message;
+
+    /**
+     * Constructs a new runtime exception with the specified detail message.
+     *
+     * @param message the detail message.
+     */
+    public NoSuchClientExistsException(String message) {
+        super(message);
+        this.message = message;
+    }
+}
