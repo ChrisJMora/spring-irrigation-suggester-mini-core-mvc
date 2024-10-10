a68671ab1a4f4cd35676a6202179c7f372c2816b
Chris <christian.jacome.mora@udla.edu.ec>
2024-10-10 00:07:07 -0500

    Update: Add new methods for finding a client by his username or email

--- src/main/java/com/example/demo/persistence/ClientRepository.java
+++ src/main/java/com/example/demo/persistence/ClientRepository.java
@@ -4,6 +4,12 @@ import com.example.demo.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> findByUsername(String username);
+    Optional<Client> findByEmail(String email);
+    Optional<Client> findByUsernameOrEmail(String username, String email);
 }
