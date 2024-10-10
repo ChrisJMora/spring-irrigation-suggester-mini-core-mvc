4047b2b16d506165c50e7871760b060beda64a98
Chris <christian.jacome.mora@udla.edu.ec>
2024-10-10 14:03:10 -0500

    Update: Add authClient method definition to ClientService

--- src/main/java/com/example/demo/service/ClientService.java
+++ src/main/java/com/example/demo/service/ClientService.java
@@ -1,15 +1,17 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Client;
+import com.example.demo.model.Credentials;
 import com.example.demo.model.WrappedEntity;
 
 import java.util.List;
 //All available CRUD methods for Users
 public interface ClientService {
-    public WrappedEntity<Client> saveClient(Client client);
-    public WrappedEntity<Client> updateClient(String username,
+    WrappedEntity<Client> saveClient(Client client);
+    WrappedEntity<Client> updateClient(String username,
                                               String email, Client client);
-    public WrappedEntity<Client> getClient(String username, String email);
-    public WrappedEntity<List<Client>> getAllClients();
-    public void deleteClient(String username, String email);
+    WrappedEntity<Client> getClient(String username, String email);
+    void authClient(Credentials credentials);
+    WrappedEntity<List<Client>> getAllClients();
+    void deleteClient(String username, String email);
 }
