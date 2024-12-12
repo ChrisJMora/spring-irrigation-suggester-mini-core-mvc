5c640853851673b3568b07d3ca0f72fe0dad249c
Chris <christian.jacome.mora@udla.edu.ec>
2024-12-12 14:30:55 -0500

    Update: SNAPSHOT

--- target/generated-sources/annotations/com/example/demo/utils/mapper/SoilMapperImpl.java
+++ target/generated-sources/annotations/com/example/demo/utils/mapper/SoilMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-12-12T14:08:51-0500",
+    date = "2024-12-12T14:29:49-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.4 (Oracle Corporation)"
 )
 @Component
